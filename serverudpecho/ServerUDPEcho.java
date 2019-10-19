34ac8dde4cea637e99beef1f7aa6f72c4a7e4c02
Veronica-Manzon-Peano-5A <56472034+Veronica-Manzon-Peano-5A@users.noreply.github.com>
2019-10-19 18:12:20 +0200

    modifica thread

--- serverudpecho/ServerUDPEcho.java
+++ serverudpecho/ServerUDPEcho.java
@@ -5,6 +5,7 @@
  */
 package serverudpecho;
 
+import java.io.IOException;
 import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,11 +19,25 @@ public class ServerUDPEcho {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int c;
+        Thread thread;
         try {
+            
             // TODO code application logic here
             UDPEcho echoServer= new UDPEcho(7);
-            echoServer.run();
+            thread= new Thread(echoServer);
+            //echoServer.start();
+            thread.start();
+            c=System.in.read();
+            //echoServer.interrupt();
+            thread.interrupt();
+            //echoServer.join();
+            thread.join();
+            System.out.println("sono il main");
+          for(;;){
+              
+          }
         } catch (SocketException ex) {
             Logger.getLogger(ServerUDPEcho.class.getName()).log(Level.SEVERE, null, ex);
         }
